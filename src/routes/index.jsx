33db4be5f95e28c289485cfb2e654bf94574ce74
huy <phamkhanhhuy1231@gmail.com>
2022-09-02 19:50:18 +0700

    19:50

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+ import Home from '~/Pages/Home'
+ import Following from '~/Pages/Following'
+ import Upload from '~/Pages/Upload'
+ import HeaderOnly from '~/ components/Layout/HeaderOnly'
+ 
+ const publicRoutes =[
+    
+    {path: '/', component: Home},
+    {path: '/following', component: Following},
+    {path: '/upload', component: Upload, layout:HeaderOnly},
+ ]
+ const privateRoutes =[
+    
+ ]
+
+ export {publicRoutes,privateRoutes}
