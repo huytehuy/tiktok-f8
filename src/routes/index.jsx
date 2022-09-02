5c1447105dda65f0d2302084a1c67f5419538d9f
huy <phamkhanhhuy1231@gmail.com>
2022-09-02 23:36:09 +0700

    fix header1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,12 @@
  import Home from '~/Pages/Home'
  import Following from '~/Pages/Following'
  import Upload from '~/Pages/Upload'
- import HeaderOnly from '~/ components/Layout/HeaderOnly'
  
  const publicRoutes =[
     
     {path: '/', component: Home},
     {path: '/following', component: Following},
-    {path: '/upload', component: Upload, layout:HeaderOnly},
+    {path: '/upload', component: Upload},
  ]
  const privateRoutes =[
     
